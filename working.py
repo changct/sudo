1509fe6dd38bfe4ce7f497c3cf3f83d0e84b517e
unknown <chang0439@gmail.com>
2024-03-30 22:21:18 +0100

    complete solve text sudo

--- working.py
+++ working.py
@@ -28,6 +28,54 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j) # row, col
+    return False
+
+def Valid(bo, num, row, col):
+    for i in range(len(bo[0])):
+        if bo[row][i] == num and i != col :
+            return False
+    
+    for i in range(len(bo)):
+        if bo[i][col] == num and i != row:
+            return False
+    
+    p_r = row // 3
+    p_c = col // 3
+
+    for i in range(p_r*3, p_r*3+3):
+        for j in range(p_c*3, p_c*3+3):
+            if bo[i][j] == num and (i != row and j != col):
+                return False
 
 
+    return True
+
+def Solve(bo):
+    #print(bo)
+    Find = find_empty(bo)
+    if not Find:
+        return True
+    else:
+        row, col = Find
+
+    
+    for i in range(1,10):
+        if Valid(bo, i, row, col):
+            bo[row][col] = i
+            if Solve(bo):
+                return True
+            bo[row][col] = 0
+    
+
+    return False
+
+
+    
+
+
+
+
+print_board(board)
+print(Solve(board))
+print("-----------")
 print_board(board)
